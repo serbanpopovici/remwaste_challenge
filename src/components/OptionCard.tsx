6590856d534adf4d1dcb79567cb2afd799eee424
Serban <serbanpopovici423@gmail.com>
2025-03-25 18:06:59 +0200

    Components structure + Styling

--- src/components/OptionCard.tsx
+++ src/components/OptionCard.tsx
@@ -16,8 +16,49 @@ const OptionCard: React.FC<Props> = (props: Props) => {
     }
 
     return (
-        <div onClick={() => handleOptionSelection()}>
-            {props.option.id}
+        <div className={`flex flex-col h-[320px] min-w-[240px] justify-between rounded-2xl ${props.option.forbidden ? 'opacity-[0.5] cursor-not-allowed' : 'hover:cursor-pointer'} ${props.selectedOption.id !== props.option.id ? 'border-2 border-gray-200' : 'border-3 border-yellow-500'}`}
+            onClick={() => handleOptionSelection()}
+        >
+            <div>
+                <div className='flex justify-between align-middle p-3 bg-gray-50 rounded-t-2xl text-xl font-bold'>
+                    <div className='flex gap-2'>
+                        <div className='h-[30px] w-[30px] text-center align-middle bg-yellow-500 rounded-lg'>{props.option.size}</div>
+                        <span>Yards</span>
+                    </div>
+                </div>
+                <div className='flex-col p-3 text-lg'>
+                    <div className='flex justify-between mb-3'>
+                        <span>
+                            Price
+                        </span>
+                        <span>
+                            £ {props.option.price_before_vat}
+                        </span>
+                    </div>
+                    <div className='flex justify-between mb-3'>
+                        <span>
+                            Hire Period
+                        </span>
+                        <span>
+                            {props.option.hire_period_days} days
+                        </span>
+                    </div>
+                    {!!!props.option.allows_heavy_waste &&
+                        <div className='size-fit px-2 mb-3 rounded-md text-red-500 border-1 border-red-500 bg-red-100'>
+                            N/A for Heavy Waste
+                        </div>
+                    }
+                    {!!!props.option.allowed_on_road &&
+                        <div className='size-fit px-2 mb-3 rounded-md text-yellow-500 border-1 border-yellow-500 bg-yellow-100'>
+                            Private Property Only
+                        </div>
+                    }
+                </div>
+            </div>
+
+            <div className={`p-2 m-3 text-center rounded-lg ${props.option.forbidden ? 'cursor-not-allowed' : 'hover:cursor-pointer hover:bg-yellow-600'} bg-yellow-500 text-xl font-bold`}>
+                {props.selectedOption.id == props.option.id ? "Selected" : "Select This Skip"}
+            </div>
         </div>
     );
 }
--- src/components/OptionsList.tsx
+++ src/components/OptionsList.tsx
@@ -8,15 +8,21 @@ const OptionsList: React.FC = () => {
     const [selectedOption, setSelectedOption] = useState<Option>({} as Option);
 
     return (
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-3'>
-            {options.map((option, index) => (
-                <OptionCard
-                    key={index}
-                    option={option}
-                    selectedOption={selectedOption}
-                    setSelectedOption={setSelectedOption}
-                />
-            ))}
+        <div className='p-4 md:px-8 lg:px-16 text-center font-mono' >
+            <div>
+                <div className='text-2xl font-bold'>Choose your skip</div>
+                <div>Select the skip size that best suits your needs</div>
+            </div>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4'>
+                {options.map((option, index) => (
+                    <OptionCard
+                        key={index}
+                        option={option}
+                        selectedOption={selectedOption}
+                        setSelectedOption={setSelectedOption}
+                    />
+                ))}
+            </div>
         </div>
     );
 };
